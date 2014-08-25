1f51b59bf26c2ea27207893177c98862f3f169a2
Marc Riegel <mail@marclab.de>
2014-08-25 12:27:57 +0200

    Added initial docs

--- setup.py
+++ setup.py
@@ -12,6 +12,20 @@ setup(
     license='MIT',
     description='Django application to delegate tasks asynchronously to worker processes.',
     long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: Software Development :: Object Brokering",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Framework :: Django",
+        "Operating System :: Unix"
+    ],
     install_requires=[
         "Django >= 1.6",
         "pika >= 0.9.0",  # Maybe this should be optional
