48a90f588121f6b08bb8afcdde5352bb440d9a58
Marc Riegel <mail@marclab.de>
2014-08-28 09:31:45 +0200

    Redesign added for docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+import alabaster
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -28,7 +29,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['alabaster']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -98,12 +99,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'github_user': 'mrcrgl',
+    'github_repo': 'django_distributed_task',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
